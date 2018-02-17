9e5ede7704711aa2dabe7d0b82b2f820bcc8bd07
Nikhil Prasad <nikp11396@gmail.com>
2018-02-17 22:31:05 +0530

    added comments

--- new_model.py
+++ new_model.py
@@ -1,7 +1,9 @@
 import tensorflow as tf, numpy as np, imageio, matplotlib.pyplot as plt, os, cv2, traceback,random
 
 
-
+"""
+Create the train and test data. Inputs are stored as paths to the images, and expected outputs as one-hot vectors.
+"""
 def create_data():
     dir_file = {'Marcel-Train/A':0, 'Marcel-Train/B':1, 'Marcel-Train/C':2, 'Marcel-Train/Five':3, 'Marcel-Train/Point':4,'Marcel-Train/V':5,'Marcel-Test/A':0, 'Marcel-Test/B':1, 'Marcel-Test/C':2, 'Marcel-Test/Five':3, 'Marcel-Test/Point':4,'Marcel-Test/V':5}
     labels = [[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,1,0],[0,0,0,0,0,1]]
@@ -19,6 +21,10 @@ def create_data():
                 test_data.append([os.path.join(dirname,filename),labels[dir_file[dirname]]])
     return train_data, test_data
 
+
+"""
+Convolutional Layer with Max Pooling and Local Response Normalization
+"""
 def conv_layer(in_layer,out_chan,size,sigma=0.01,b=0.01,strd=[1,1,1,1],pool=True):
     in_chan = in_layer.shape.as_list()[3]
     w = tf.Variable(tf.truncated_normal([size,size,in_chan,out_chan],stddev=sigma))
@@ -31,6 +37,10 @@ def conv_layer(in_layer,out_chan,size,sigma=0.01,b=0.01,strd=[1,1,1,1],pool=True
         return w,b,h,p,n
     return w,b,h,n1
 
+
+"""
+Fully Connected Layer
+"""
 def conn_layer(in_layer,out_nodes,op_layer=False,sigma=0.1,b=0.0):
     i_s = in_layer.shape.as_list()
     print(i_s)
@@ -46,6 +56,10 @@ def conn_layer(in_layer,out_nodes,op_layer=False,sigma=0.1,b=0.0):
         h = tf.nn.relu(h)
     return w,b,h
 
+
+"""
+The architecture: 3 conv layers and  2 fc layers with dropout
+"""
 x = tf.placeholder(tf.float32, shape=[None,76*66*3])
 y = tf.placeholder(tf.float32, shape=[None,6])
 learning_rate = tf.placeholder(tf.float32)
@@ -61,13 +75,31 @@ h5_drop = tf.nn.dropout(h5,keep_prob)
 w6,b6,y_ = conn_layer(h5_drop,6,op_layer=True)
 
 
-                        
+"""
+Loss function: Softmax Cross Entropy
+"""
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_))
+
+"""
+Adaptive moments for training
+"""
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
+
+"""
+Compare predicted classes vs actual classes
+"""
 correct_prediction = tf.cast(tf.equal(tf.argmax(y,1),tf.argmax(y_,1)),tf.float32)
+
+"""
+Saver object to save and restore variables
+"""
 saver = tf.train.Saver({'w1':w1,'b1':b1,'w2':w2,'b2':b2,'w3':w3,'b3':b3,'w4':w4,'b4':b4,'w5':w5,'b5':b5,'w6':w6,'b6':b6})
 
+
+"""
+Get training data in a sequential manner
+"""
 def get_batch(start,end,files):
     train_array = [cv2.resize(imageio.imread(file)[:,:,:3],(66,76),interpolation=cv2.INTER_AREA).reshape(76*66*3) for file,lab in files[start:end]]
     train_labels = [lab for file,lab in files[start:end]]
@@ -75,6 +107,10 @@ def get_batch(start,end,files):
     #print(train_labels)
     return [train_array, train_labels]
 
+
+"""
+Get training data randomly
+"""
 def get_batch_random(files, batch_sz, f_read_dict):
     train_array = []
     train_labels = []
@@ -87,6 +123,10 @@ def get_batch_random(files, batch_sz, f_read_dict):
         if len(f_read_dict) == 2400:
             f_read_dict = {}
     return [train_array, train_labels]
+
+"""
+Visualize output of a convolutional layer
+"""
 def visualize_layer(layer,sess,feed):
     unit = sess.run(layer,feed_dict = feed)
 ##    m = unit[0][0][0][0]
@@ -100,7 +140,9 @@ def visualize_layer(layer,sess,feed):
     cv2.waitKey(1)
 
 
-
+"""
+check validation accuracy
+"""
 def validate(test_data):
     acc = 0
     ls2 = 0
@@ -115,6 +157,9 @@ def validate(test_data):
     except:
         traceback.print_exc()
 
+"""
+Train the model. Inputs: number of epochs, learning rate, train and test data, and whether to continue training model or start afresh
+"""
 def train(epochs,batch_sz,epsilon,train_data,test_data,reload=False):
     ls = []
     ls2 = []
@@ -124,7 +169,7 @@ def train(epochs,batch_sz,epsilon,train_data,test_data,reload=False):
         sess.run(tf.global_variables_initializer())
         if reload:
             try:
-                saver.restore(sess, 'F:/Work/repos/Gesture-Classifier/model6.ckpt')
+                saver.restore(sess, 'model6.ckpt')
             except:
                 print("Error restoring checkpoint; starting afresh")
                 
@@ -140,7 +185,7 @@ def train(epochs,batch_sz,epsilon,train_data,test_data,reload=False):
                 a,l = validate(test_data)
                 acc.append(a)
                 ls2.append(l)
-        save_path = saver.save(sess, "F:/Work/repos/Gesture-Classifier/model6.ckpt")
+        save_path = saver.save(sess, "model6.ckpt")
         print("Model saved at ", save_path)
         x1 = [i for i in range(len(ls))]
         x2 = [i for i in range(len(acc))]
@@ -153,7 +198,9 @@ def train(epochs,batch_sz,epsilon,train_data,test_data,reload=False):
         plt.plot(x3,ls2)
         plt.show()
 
-        
+"""
+Test the model without training.
+"""
 def test():
     dir_file = {'Marcel-Train/A':0, 'Marcel-Train/B':1, 'Marcel-Train/C':2, 'Marcel-Train/Five':3, 'Marcel-Train/Point':4,'Marcel-Train/V':5,'Marcel-Test/A':0, 'Marcel-Test/B':1, 'Marcel-Test/C':2, 'Marcel-Test/Five':3, 'Marcel-Test/Point':4,'Marcel-Test/V':5}
     labels = [[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,0,0],[0,0,0,1,0,0],[0,0,0,0,1,0],[0,0,0,0,0,1]]
@@ -163,7 +210,7 @@ def test():
             if filename.endswith('jpg'):
                 test_data.append([os.path.join(dirname,filename),labels[dir_file[dirname]]])
     with tf.Session() as sess:
-        saver.restore(sess, 'F:/Work/repos/Gesture-Classifier/model5.ckpt')
+        saver.restore(sess, 'model6.ckpt')
         acc = 0
         for file, lab in test_data:
             img = cv2.resize(imageio.imread(file)[:,:,:3],(66,76),interpolation=cv2.INTER_AREA).reshape(76*66*3)
@@ -172,10 +219,14 @@ def test():
             acc += correct_prediction.eval(feed_dict={x:[img], y:[lab],keep_prob:1.0})
         acc/=len(test_data)
     print(acc)
+
+"""
+Set up the data and start training or testing.
+"""
 train_data,test_data = create_data()
 print('train data imgs: ',len(train_data))
 print('test data imgs: ',len(test_data))
-view_img = cv2.resize(imageio.imread('F:/Work/repos/Gesture-Classifier/Marcel-Test/A/A-uniform01.jpg')[:,:,:3],(66,76),interpolation=cv2.INTER_AREA).reshape(76*66*3)
+view_img = cv2.resize(imageio.imread('Marcel-Test/A/A-uniform01.jpg')[:,:,:3],(66,76),interpolation=cv2.INTER_AREA).reshape(76*66*3)
 train(200,10,1e-4,train_data,test_data,reload=True)
 #test()
 
