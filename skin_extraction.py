79e09691d03d55558f77e3d8f5d5e936bfd1e319
Satya Teja Chikatla <turrentrock@gmail.com>
2018-03-06 18:33:36 +0530

    New Data wit h different approach

--- skin_extraction.py
+++ skin_extraction.py
@@ -12,14 +12,14 @@ while True:
 		im_ycrcb = cv2.cvtColor(im, cv2.COLOR_BGR2YCR_CB)
 
 		skin_ycrcb = cv2.inRange(im_ycrcb, skin_ycrcb_mint, skin_ycrcb_maxt)
-		cv2.imshow('skin_ycrcb',skin_ycrcb[400:600,400:600]) # Second image
+		cv2.imshow('skin_ycrcb',skin_ycrcb[300:300+256,200:200+256]) # Second image
 
 		image, contours, hierarchy = cv2.findContours(skin_ycrcb, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		for i, c in enumerate(contours):
 			area = cv2.contourArea(c)
 			if area > 1000:
 				cv2.drawContours(im, contours, i, (255, 0, 0), 3)
-		cv2.imshow('im', im[400:600,400:600,:])
+		cv2.imshow('im', im[300:300+256,200:200+256,:])
 
 		k = cv2.waitKey(30) & 0xff
 		if k == 27:
--- withmog.py
+++ withmog.py
@@ -4,14 +4,14 @@ import cv2
 #cap = cv2.VideoCapture('people-walking.mp4')
 fgbg = cv2.createBackgroundSubtractorMOG2()
 #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-#history=10
+history=5
 cap = cv2.VideoCapture(0)
 
 while(1):
 	ret, frame = cap.read()
 	if ret == True:
-		fgmask = fgbg.apply(frame)
-		#fgmask = fgbg.apply(frame,learningRate=1.0/history)
+		#fgmask = fgbg.apply(frame)
+		fgmask = fgbg.apply(frame,learningRate=1.0/history)
 		#fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 		
 		cv2.imshow('fgmask',frame)
